03902513cfb6dfb695b92b94519297b1af99f31c
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 16:41:21 -0300

    fix: change margin to padding GenericMargin

--- src/templates/GenericMargin.tsx
+++ src/templates/GenericMargin.tsx
@@ -1,13 +1,23 @@
 import { ReactNode } from "react";
 
-const genericMarginStyles = {margin: "0 128px"};
+const genericMarginStyles = {padding: "0 128px"};
 
 type GenericMarginProps = {
   children: ReactNode,
+  classname?: string,
 }
 
 const GenericMargin = (props: GenericMarginProps) => {
   const { children } = props;
+
+  if (props.classname) return (
+    <div 
+      style={genericMarginStyles}
+      className={props.classname}
+    >
+      { children }
+    </div>
+  );
 
   return (
     <div style={genericMarginStyles}>
