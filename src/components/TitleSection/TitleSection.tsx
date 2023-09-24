861704affd478eb22df5b772bca53419550910f8
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 00:49:23 -0300

    feat: TitleSection structure

---
+++ src/components/TitleSection/TitleSection.tsx
@@ -0,0 +1,12 @@
+import TitleSectionStyles from "./TitleSection.module.scss";
+
+const TitleSection = () => {
+  return (
+    <section className={TitleSectionStyles.titleSectionWrapper}>
+      <h1>Wait ! your order in progress.</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing </p>
+    </section>
+  );
+};
+
+export default TitleSection;
