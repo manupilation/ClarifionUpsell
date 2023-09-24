d8cdd2a8546e40bb89786c6389586c7a01702057
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 13:29:11 -0300

    feat: incomplete product scheme

---
+++ src/elements/Product/Product.tsx
@@ -0,0 +1,26 @@
+const Product = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+
+      <div className="provisory-noImageWrapper">
+        <div className="provisory-TitleWrapper">
+          <h4>Clarifion Air Ionizer</h4>
+
+          <div>
+            <p>$180</p>
+            <p>$84</p>
+          </div>
+        </div>
+
+        {/* Five stars svg here */}
+
+        <p className="provisory-singleList">12 left in Stock</p>
+
+        <p>Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
