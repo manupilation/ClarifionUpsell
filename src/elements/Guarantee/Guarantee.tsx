8ede468f87f25ba3862247f9ba602546c5c00d98
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 13:28:55 -0300

    feat: guarantee section

---
+++ src/elements/Guarantee/Guarantee.tsx
@@ -0,0 +1,10 @@
+const Guarantee = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <p>If you are not completely thrilled with your Clarifion - We have a 30 day satisfaction guarantee. Please refer to our return policy at the bottom of the page for more details. Happy Shopping!</p>
+    </div>
+  );
+};
+
+export default Guarantee;
