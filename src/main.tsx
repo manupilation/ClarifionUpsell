fba7fa94bd4dce202d54bd7ed63794310cd5345a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 14:02:09 -0300

    feat: import globals

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Page from "./Page.tsx";
+import "./styles/globalStyles.css";
+import "./styles/colors.scss";
+import "./styles/fonts.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
