4696242f2440775554db4ad021568f95ab7a3396
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 03:21:17 -0300

    feat: page using BrandBoxes, Title and StepList

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,9 +1,15 @@
+import BrandBoxes from "./components/BrandBoxes/BrandBoxes";
 import Header from "./components/Header/Header";
+import StepList from "./components/StepList/StepList";
+import TitleSection from "./components/TitleSection/TitleSection";
 
 function App() {
   return (
     <>
       <Header />
+      <BrandBoxes />
+      <TitleSection />
+      <StepList />
     </>
   );
 }
