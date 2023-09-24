72323e3d7da8d45082479af4be8a456d15cfe84a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-24 13:28:34 -0300

    feat: incomplete discount section

---
+++ src/elements/DiscountSection/DiscountSection.tsx
@@ -0,0 +1,28 @@
+const DiscountSection = () => {
+  return (
+    <div>
+      <p></p>
+
+      <button type="button">Yes - Claim my discount</button>
+
+      <div>
+        <p>Free shipping</p>
+        <p>Secure 256-bit SSL encryption.</p>
+
+        <div>
+          <p>visa</p>
+          <p>visa</p>
+          <p>visa</p>
+          <p>visa</p>
+          <p>visa</p>
+          <p>visa</p>
+          <p>visa</p>
+        </div>
+      </div>
+
+      <button type="button">No thanks, I don`t want this.</button>
+    </div>
+  );
+};
+
+export default DiscountSection;
