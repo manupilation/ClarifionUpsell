20d5767aeaadbef37d288f9c7585abdfbb3b3aa0
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 00:20:24 -0300

    feat: BenefitList

--- src/components/BenefitList/BenefitList.tsx
+++ src/components/BenefitList/BenefitList.tsx
@@ -1,9 +1,10 @@
 import benefits from "../../constants/Benefits";
 import BenefitComponent from "../../elements/Benefit/Benefit";
+import BenefitListStyles from "./BenefitList.module.scss";
 
 const BenefitList = () => {
   return (
-    <ul>
+    <ul className={BenefitListStyles.benefitsWrapper}>
       {benefits.map(({text, key}) => <BenefitComponent text={text} key={key}/>)}
     </ul>
   );
