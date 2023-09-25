7aa999cfe1edf9b9033ddb6206d6549f095f9e92
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 00:20:34 -0300

    feat: Body section

--- src/components/BodySection/BodySection.tsx
+++ src/components/BodySection/BodySection.tsx
@@ -1,6 +1,5 @@
 import GenericMargin from "../../templates/GenericMargin";
 import BodySectionStyles from "./BodySection.module.scss";
-import ClarifionProduct from "../../../public/images/ClarifionProduct.png";
 import ProductInfoSection from "../ProductInfoSection/ProductInfoSection";
 import Profile from "../../elements/Profile/Profile";
 
@@ -8,7 +7,6 @@ const BodySection = () => {
   return (
     <GenericMargin classname={BodySectionStyles.bodySectionContainer}>
       <div className={BodySectionStyles.bodySectionWrapper}>
-        <img src={ClarifionProduct} alt="" />
 
         <Profile classname={BodySectionStyles.profileWrapper}/>
 
