e3e7abbb0888a9d8b7909fd4dabe285e4b5dda7a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 00:22:10 -0300

    refactor: provile with image

--- src/elements/Profile/Profile.tsx
+++ src/elements/Profile/Profile.tsx
@@ -1,22 +1,24 @@
 import KenImage from "../../../public/images/KenProfile.png";
 import Stars from "../../../public/svgs/Stars.svg";
 import VerifiedCheck from "../../../public/svgs/VerifiedCostumer.svg";
+import ClarifionProduct from "../../../public/images/ClarifionProduct.png";
 
 const Profile = (props: {classname: string}) => {
   const {classname} = props;
 
   return (
     <div className={classname}>
+      <img src={ClarifionProduct} alt="" />
       <div>
         <img src={KenImage} alt="Ken smiling." />
-
-        <img src={Stars} alt="" />
-
-        <p>Ken T.</p>
-
-        <img src={VerifiedCheck} alt="" />
+        <div>
+          <img src={Stars} alt="" />
+          <div>
+            <p>Ken T.</p>
+            <img src={VerifiedCheck} alt="" />
+          </div>
+        </div>
       </div>
-      
 
       <p>“As soon as the Clarifions arrived I put one in my bedroom. This was late in the afternoon. When I went to the bedroom in the evening it smelled clean. When I went to bed I felt I could breathe better. Wonderful.”</p>
     </div>
