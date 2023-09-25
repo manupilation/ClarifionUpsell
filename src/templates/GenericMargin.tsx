08cd89db4868b29bed0fc0e5bcab1e3464047fba
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 00:22:44 -0300

    feat: Generic margin detect screen width

--- src/templates/GenericMargin.tsx
+++ src/templates/GenericMargin.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 const genericMarginStyles = {padding: "0 128px"};
+const genericMarginStylesMinor = {padding: "0 64px"};
+const genericMarginStylesMobile = {padding: "0 20px"};
 
 type GenericMarginProps = {
   children: ReactNode,
@@ -9,12 +11,38 @@ type GenericMarginProps = {
 
 const GenericMargin = (props: GenericMarginProps) => {
   const { children } = props;
+  const [width, setWidth] = useState(0);
 
   if (props.classname) return (
     <div 
       style={genericMarginStyles}
       className={props.classname}
     >
+      { children }
+    </div>
+  );
+
+  const updateWindowDimensions = () => {
+    const newWidth = window.innerWidth;
+    setWidth(newWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateWindowDimensions);
+
+    return () => {
+      window.removeEventListener("resize", updateWindowDimensions);
+    };
+  }, []);
+
+  if (width < 1200) return (
+    <div style={genericMarginStylesMinor}>
+      { children }
+    </div>
+  );
+
+  if (width < 700) return (
+    <div style={genericMarginStylesMobile}>
       { children }
     </div>
   );
