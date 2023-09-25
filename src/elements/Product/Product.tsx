16289631be9d4f99888077fa02644ad3186b1b1a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 00:21:54 -0300

    feat: product component

--- src/elements/Product/Product.tsx
+++ src/elements/Product/Product.tsx
@@ -1,25 +1,28 @@
 import ClarifionP from "../../../public/images/Clarifion.png";
+import ProductStyles from "./Product.module.scss";
+import FiveStars from "../../../public/svgs/RoundStars.svg";
+import Dot from "../../../public/svgs/Dot.svg";
 
 const Product = () => {
   return (
-    <div>
+    <div className={ProductStyles.productWrapper}>
       <img src={ClarifionP} alt="" />
 
-      <div className="provisory-noImageWrapper">
-        <div className="provisory-TitleWrapper">
+      <div className={ProductStyles.productDetails}>
+        <div>
           <h4>Clarifion Air Ionizer</h4>
 
-          <div>
+          <div className={ProductStyles.productPrices}>
             <p>$180</p>
-            <p>$84</p>
+            <span>$84</span>
           </div>
         </div>
 
-        {/* Five stars svg here */}
+        <img src={FiveStars} alt="" className={ProductStyles.productRate}/>
 
-        <p className="provisory-singleList">12 left in Stock</p>
+        <p className={ProductStyles.stock}><img src={Dot} alt="" />12 left in Stock</p>
 
-        <p>Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.</p>
+        <p className={ProductStyles.explanation}>Simply plug a Clarifion into any standard outlet and replace bulky, expensive air purifiers with a simple.</p>
       </div>
     </div>
   );
