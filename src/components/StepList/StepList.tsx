c0e2cbda100572cbd2db82106498bc488b8bcd74
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-09-25 13:26:52 -0300

    feat: implement mobile list

--- src/components/StepList/StepList.tsx
+++ src/components/StepList/StepList.tsx
@@ -1,10 +1,32 @@
+import { useContext } from "react";
 import steps from "../../constants/Steps";
 import Step from "../../elements/Step/Step";
 import GenericMargin from "../../templates/GenericMargin";
 import StepType from "../../types/StepType";
 import StepListStyles from "./StepList.module.scss";
+import { mobileContext } from "../../context/mobileContext";
+import mobileSteps from "../../constants/MobileSteps";
 
 const StepList = () => {
+  const {isMobile} = useContext(mobileContext);
+
+  if (isMobile) {
+    return (
+      <GenericMargin>
+        <div className={StepListStyles.stepWrapper}>
+          {
+            mobileSteps.map((step: StepType) => 
+              <Step
+                text={step.text}
+                className={StepListStyles.stepComponent}
+                key={step.key}
+              />)
+          }
+        </div>
+      </GenericMargin>
+    );
+  }
+
   return (
     <GenericMargin>
       <div className={StepListStyles.stepWrapper}>
